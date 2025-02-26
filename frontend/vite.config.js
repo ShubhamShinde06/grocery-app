f0b8087dc2d475fdf621188ac3cea513917368d7
ShubhamShinde06 <officialshubhamorg@gmail.com>
2025-02-26 20:35:29 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://localhost:8000'
-    }
+  build: {
+    chunkSizeWarningLimit: 1000 // Set higher limit (default is 500KB)
   },
   plugins: [
     react(),
